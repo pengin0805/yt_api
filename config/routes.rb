f8d7332ef4f063be8ea5f05ece2efce29eb05996
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-20 14:34:52 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: "search#index"
+  get 'search', to: 'search#search'
+  post 'import', to: 'search#import'
+    
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
